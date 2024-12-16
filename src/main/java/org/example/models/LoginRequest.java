955e2f2524994c0ebb23171eb7f266ff8d28106e
Matthew Jordan <matthew.jordan@kainos.com>
2024-12-16 16:54:00 +0000

    US040: Added setters to request models

--- src/main/java/org/example/models/LoginRequest.java
+++ src/main/java/org/example/models/LoginRequest.java
@@ -20,7 +20,15 @@ public class LoginRequest {
         return email;
     }
 
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
     }
 }
--- src/main/java/org/example/models/RegisterRequest.java
+++ src/main/java/org/example/models/RegisterRequest.java
@@ -20,7 +20,15 @@ public class RegisterRequest {
         return email;
     }
 
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
     }
 }
