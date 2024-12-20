bcf813d55591ae764ded59425eda454b495b41d1
dylanmckk <dylan.mckenzie@kainos.com>
2024-12-20 11:55:48 +0000

    Removed logging from tests

--- src/test/java/org/example/integration/JobRoleIntegrationTest.java
+++ src/test/java/org/example/integration/JobRoleIntegrationTest.java
@@ -8,31 +8,23 @@ import org.example.models.JobRole;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Client;
 import java.util.List;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class JobRoleIntegrationTest {
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(JobRoleIntegrationTest.class);
-
     public static final DropwizardAppExtension<TestConfiguration> APP =
             new DropwizardAppExtension<>(TestApplication.class);
 
     @Test
     void getJobRoles_shouldReturnListOfJobRoles() {
-        LOGGER.info("Initialised Integration Test for GET /job-roles");
         Client client = APP.client();
 
         List<JobRole> response = client
                 .target("http://localhost:8080/job-roles")
                 .request()
                 .get(List.class);
-
-        LOGGER.debug("Response received {}", response);
 
         Assertions.assertFalse(response.isEmpty());
     }
