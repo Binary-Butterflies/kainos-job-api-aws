3e30ae5df508da6fffc774c8f3aa771c0846ae79
dylanmckk <dylan.mckenzie@kainos.com>
2024-12-20 11:55:48 +0000

    Removed logging from tests

--- src/test/java/org/example/controllers/JobRoleControllerTest.java
+++ src/test/java/org/example/controllers/JobRoleControllerTest.java
@@ -6,8 +6,6 @@ import org.example.models.JobRoleResponse;
 import org.example.services.JobRoleService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
@@ -17,9 +15,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 public class JobRoleControllerTest {
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(JobRoleControllerTest.class);
-
     JobRoleService jobRoleService = Mockito.mock(JobRoleService.class);
     JobRoleController jobRoleController = new JobRoleController(jobRoleService);
 
@@ -34,9 +29,7 @@ public class JobRoleControllerTest {
 
     @Test
     void getAllJobRoles_ReturnsJobRoles_ReturnTwoHundred() throws SQLException {
-        LOGGER.info("Initialised Unit Test for getAllJobRoles_ReturnsJobRoles_ReturnTwoHundred");
         when(jobRoleService.getAllJobRoles()).thenReturn(jobRoleResponse);
-        LOGGER.debug("Mocked jobRoleService to return {}", jobRoleResponse);
 
         Response response = jobRoleController.getJobRoles();
 
@@ -46,9 +39,7 @@ public class JobRoleControllerTest {
 
     @Test
     void getAllJobRoles_ThrowSqlException_ReturnFiveHundred() throws SQLException {
-        LOGGER.info("Initialised Unit Test for getAllJobRoles_ThrowSqlException_ReturnFiveHundred");
         when(jobRoleService.getAllJobRoles()).thenThrow(new SQLException());
-        LOGGER.debug("Mocked jobRoleService to throw SQLException");
 
         Response response = jobRoleController.getJobRoles();
 
