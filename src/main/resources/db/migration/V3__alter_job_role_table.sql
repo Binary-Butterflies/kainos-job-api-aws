1344832c3e735e759e072355b70840e17b197d0d
Matthew Jordan <matthew.jordan@kainos.com>
2025-01-10 11:57:45 +0000

    US002: Updated Migration Script

--- src/main/resources/db/migration/V3__alter_job_role_table.sql
+++ src/main/resources/db/migration/V3__alter_job_role_table.sql
@@ -4,10 +4,24 @@ CREATE TABLE status (
     CONSTRAINT pk_status PRIMARY KEY (statusId)
 );
 
+INSERT INTO status(statusId, statusName) VALUES (1, "Open");
+INSERT INTO status(statusId, statusName) VALUES (2, "Closed");
+
 ALTER TABLE job_roles
-    ADD description VARCHAR(512) NOT NULL,
-    ADD responsibilities VARCHAR(512) NOT NULL,
-    ADD sharepointUrl VARCHAR(512) NOT NULL,
-    ADD statusId INT NOT NULL,
-    ADD numberOfOpenPositions INT NOT NULL,
-    ADD CONSTRAINT fk_statusId FOREIGN KEY (statusId) REFERENCES status(statusId);
+ADD description VARCHAR(512) NOT NULL,
+ADD responsibilities VARCHAR(512) NOT NULL,
+ADD sharepointUrl VARCHAR(512) NOT NULL,
+ADD statusId INT NOT NULL,
+ADD numberOfOpenPositions INT NOT NULL;
+
+-- Disable safe updates temporarily in order to set fields for existing jobs
+SET SQL_SAFE_UPDATES = 0;
+UPDATE `job_roles` SET description = "";
+UPDATE `job_roles` SET responsibilities = "";
+UPDATE `job_roles` SET sharepointUrl = "";
+UPDATE `job_roles` SET statusId = 1;
+UPDATE `job_roles` SET numberOfOpenPositions = 1;
+SET SQL_SAFE_UPDATES = 1;
+
+ALTER TABLE job_roles
+ADD CONSTRAINT fk_statusId FOREIGN KEY (statusId) REFERENCES status(statusId);
